f2fa7250643eb38a10ebc8d005eff9bbf9fd947d
Timi16 <timilehinolowu46@gmail.com>
2024-05-21 09:51:06 +0100

    Adding webRtc feature for test

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -7,47 +6,55 @@ const sellerRoutes = require('./routes/sellerRoutes');
 const productRoutes = require('./routes/productRoutes');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
+const path = require('path');
 const app = express();
-//const webtrtc = require('wrtc');
 const server = http.createServer(app);
 const io = socketIo(server);
-const cors = require('cors');
+let broadcaster;
 app.use(cors());
-// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// Connect to MongoDB directly (for testing purposes)
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 mongoose.connect("mongodb+srv://Admin:admin12345@atlascluster.am2nbgm.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-// Routes
 app.use('/api', userRoutes);
 app.use('/api', sellerRoutes);
 app.use('/api', productRoutes);
-// Socket.io connection handling
-//app.post('/broadcast',async({body}) , res) => {
-    //const peer = new webtrc.RTCPeerConnection({
-     //   iceServers : [
-         //    {
-    
+
+
 io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
+    console.log('A user connected:', socket.id);
 
-  // Handle incoming messages
-  socket.on('sendMessage', (data) => {
-    io.to(data.receiverSocketId).emit('message', {
-      senderId: data.senderId,
-      message: data.message
+    socket.on('broadcaster', () => {
+        broadcaster = socket.id;
+        socket.broadcast.emit('broadcaster');
     });
-  });
 
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
-  });
+    socket.on('watcher', () => {
+        socket.to(broadcaster).emit('watcher', socket.id);
+    });
+
+    socket.on('offer', (id, message) => {
+        socket.to(id).emit('offer', socket.id, message);
+    });
+
+    socket.on('answer', (id, message) => {
+        socket.to(id).emit('answer', socket.id, message);
+    });
+
+    socket.on('candidate', (id, message) => {
+        socket.to(id).emit('candidate', socket.id, message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+        socket.to(broadcaster).emit('disconnectPeer', socket.id);
+    });
 });
-
-// Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
